15a1d87cc1b6003a72787e5f742f1501af46c2d4
SafronovaOlga <104982522+SafronovaOlga@users.noreply.github.com>
2025-02-17 10:40:00 +0100

    Created new clsses for calculator (#1)

--- src/main/java/safronova/Main.java
+++ src/main/java/safronova/Main.java
@@ -3,15 +3,74 @@ package safronova;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main{
-    public static void main( String[] args ){
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf( "Hello and welcome!" );
         
-        for( int i=1;i<=5;i++ ){
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println( "i = "+i );
+        public static void main(String[] args) {
+            // Пример вызова функции
+            double distance = 25; // Расстояние в км
+            String size = "большие"; // Габариты груза
+            boolean isFragile = true; // Хрупкость груза
+            String workload = "высокая"; // Загруженность службы доставки
+            
+            double cost = calculateDeliveryCost(distance, size, isFragile, workload);
+            System.out.println("Стоимость доставки: " + cost + " рублей");
+            // Стоимость доставки: 979.9999999999999 рублей
         }
-    }
+        
+        public static double calculateDeliveryCost(double distance, String size, boolean isFragile, String workload) {
+            double baseCost = 0;
+            
+            // Расчет стоимости в зависимости от расстояния
+            if (distance > 30) {
+                baseCost += 300;
+            } else if (distance > 10) {
+                baseCost += 200;
+            } else if (distance > 2) {
+                baseCost += 100;
+            } else {
+                baseCost += 50;
+            }
+            
+            // Проверка на хрупкость и расстояние
+            if (isFragile && distance > 30) {
+                throw new IllegalArgumentException("Хрупкие грузы нельзя возить на расстояние более 30 км");
+            }
+            
+            // Расчет стоимости в зависимости от габаритов
+            if (size.equals("большие")) {
+                baseCost += 200;
+            } else if (size.equals("маленькие")) {
+                baseCost += 100;
+            }
+            
+            // Добавление стоимости за хрупкость
+            if (isFragile) {
+                baseCost += 300;
+            }
+            
+            // Применение коэффициента загруженности
+            double workloadCoefficient = 1.0;
+            switch (workload) {
+                case "очень высокая":
+                    workloadCoefficient = 1.6;
+                    break;
+                case "высокая":
+                    workloadCoefficient = 1.4;
+                    break;
+                case "повышенная":
+                    workloadCoefficient = 1.2;
+                    break;
+                default:
+                    workloadCoefficient = 1.0;
+            }
+            
+            double totalCost = baseCost * workloadCoefficient;
+            
+            // Проверка на минимальную стоимость доставки
+            if (totalCost < 400) {
+                totalCost = 400;
+            }
+            
+            return totalCost;
+        }
+    
 }
