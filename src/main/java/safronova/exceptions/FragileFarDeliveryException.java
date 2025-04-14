969edc75452d62e5f37b1c129b95c93508dab881
new_name <new_email>
2025-04-14 15:41:35 +0200

    Modified and updated tests classes

---
+++ src/main/java/safronova/exceptions/FragileFarDeliveryException.java
@@ -0,0 +1,1 @@
+package safronova.exceptions;public class FragileFarDeliveryException extends RuntimeException{        public FragileFarDeliveryException(String message) {                super(message);    }        @Override    public String toString(){        return "FragileFarDeliveryException { " + "message: " + getMessage( ) + " }";    }        }
